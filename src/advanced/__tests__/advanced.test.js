6961260218e0a2549af5977fe7c7805d510344cc
유용태 <teo.v@navercorp.com>
2025-04-11 17:48:39 +0900

    - packages 최신화 - vitest config 수정 - Readme 수정

--- src/advanced/__tests__/advanced.test.js
+++ src/advanced/__tests__/advanced.test.js
@@ -1,5 +1,7 @@
-import {describe} from "vitest";
+import {describe, expect, it} from "vitest";
 
 describe('advanced test', () => {
-
+  it('테스트코드를 자유롭게 한번 연습해보세요.', () => {
+    expect(true).toBe(true);
+  });
 });
