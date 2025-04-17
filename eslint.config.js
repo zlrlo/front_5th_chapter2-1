c2d4af74337dd50dfa8de2b2462116dfcf49bb2d
zlrlo <easy10612@gmail.com>
2025-04-17 21:16:17 +0900

    chore: ESLint 및 Prettier 설정 추가, 관련 패키지 설치

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default defineConfig([
+  // JavaScript 기본 룰
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    ignores: ["src/main.original.js"],
+  },
+  // 브라우저 환경 전역 변수 허용
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  // Prettier 연동: 포맷팅 오류를 ESLint로 잡기
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
+  // TypeScript 기본 룰
+  tseslint.configs.recommended,
+  // React 기본 룰
+  pluginReact.configs.flat.recommended,
+  // Prettier 룰
+  prettierConfig,
+]);
