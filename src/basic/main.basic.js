4b36ea97a719057bda0b4d99b54d9085b4e95a6f
zlrlo <easy10612@gmail.com>
2025-04-17 23:40:42 +0900

    refactor: 상수들을 별도의 파일로 분리

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -1,14 +1,8 @@
-const BULK_DISCOUNT_THRESHOLD = 30;
-
-const BULK_DISCOUNT_RATE = 0.25;
-
-const PRODUCT_DISCOUNT_RATE = {
-  p1: 0.1,
-  p2: 0.15,
-  p3: 0.2,
-  p4: 0.05,
-  p5: 0.25,
-};
+import {
+  BULK_DISCOUNT_THRESHOLD,
+  BULK_DISCOUNT_RATE,
+  PRODUCT_DISCOUNT_RATE,
+} from "../constants";
 
 const productList = [
   { id: "p1", name: "상품1", value: 10000, quantity: 50 },
---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+export const BULK_DISCOUNT_THRESHOLD = 30;
+
+export const BULK_DISCOUNT_RATE = 0.25;
+
+export const PRODUCT_DISCOUNT_RATE = {
+  p1: 0.1,
+  p2: 0.15,
+  p3: 0.2,
+  p4: 0.05,
+  p5: 0.25,
+};
